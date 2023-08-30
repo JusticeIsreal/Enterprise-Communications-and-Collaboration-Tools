d282f9360c7176845ae75e364ee951f344aa2797
Justice Isreal <justiceyba@gmail.com>
2023-08-30 14:29:56 +0100

    Prisma , mysql and schema for profile, channel, server and member completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 const inter = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,11 +19,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className={inter.className}>
+        <body className={cn(inter.className, "bg-white dark:bg-[#313338]")}>
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
-            enableSystem={false}
+            enableSystem={true}
             storageKey="dischord-theme"
           >
             {children}
